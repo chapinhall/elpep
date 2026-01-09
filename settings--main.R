6773a6050cc18d0baefdd889e99a2c8cf7ac9488
Nick Mader <mader-nick1@norc.org>
2026-01-09 17:52:46 -0500

    Added packages used in maps telling the story of ethe progression of estimates (in 03c)

--- settings--main.R
+++ settings--main.R
@@ -16,7 +16,7 @@ packages.list <-
     "extrafont", "remotes", "broom", "sf", "lwgeom", "foreach", "doParallel",
     "tidycensus", "fredr", "ipumsr", "tsibble", "forecast", "fable", #"xlsx", 
     "boot", "ggtext", "data.table", "tidyr", "dplyr", "scales", "janitor", "R.utils",
-    "lubridate", "readxl", "openxlsx", "officer")
+    "lubridate", "readxl", "openxlsx", "officer", "spatial", "ggthemes")
 
 for (p in packages.list) {
   if (!p %in% installed.packages()[, "Package"]) {
